01d632e285e1ee29ca06d96c2383525ce21138a9
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2023-04-18 01:20:01 +1000

    fix: make String.exec more resilient—handle arguments with spaces and across OS'

--- src/main/kotlin/com/gtramontina/ghooks/String.exec.kt
+++ src/main/kotlin/com/gtramontina/ghooks/String.exec.kt
@@ -1,16 +1,21 @@
 package com.gtramontina.ghooks
 
 import java.io.File
+import java.lang.System.getProperty
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletableFuture.supplyAsync
 import java.util.concurrent.TimeUnit.SECONDS
 
 private const val SUCCESSFUL = 0
-private val BLANK_SPACES = "\\s".toRegex()
 
 internal fun String.exec(cwd: File, timeoutInSeconds: Long = 60): CompletableFuture<String> = supplyAsync {
-    ProcessBuilder(*split(BLANK_SPACES).toTypedArray())
-        .redirectErrorStream(true)
+    val process = if (getProperty("os.name").startsWith("Windows")) {
+        ProcessBuilder("cmd", "/C", this)
+    } else {
+        ProcessBuilder("/bin/sh", "-c", this)
+    }
+
+    process.redirectErrorStream(true)
         .directory(cwd)
         .start()
         .apply { waitFor(timeoutInSeconds, SECONDS) }
